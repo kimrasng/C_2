9cafbc7c29d1ead7578a610a787bfbc0d97bb28c
김레생 <85009117+kimrasng@users.noreply.github.com>
2023-05-14 11:59:40 +0900

    qwe

--- 0000/0000/0000.c
+++ 0000/0000/0000.c
@@ -1,5 +1,4 @@
 //	예제 모음01
-#include	<stdio.h>
 
 /*
 int main(void)
@@ -41,6 +40,7 @@ int main(void)
 
 
 //	예제 모음03
+/*
 #include	<stdio.h>
 
 int main(void)
@@ -60,11 +60,135 @@ int main(void)
 
 	return 0;
 }
- 
+ */
  
 //	예제 모음04
+
+/*
+#include	<stdio.h>
+
+int main(void)
+{
+	int a, c;
+	char b;
+	
+	printf("첫번쨰 수를 입력하세요 : ");
+	scanf_s("%d", &a);
+	printf("계산할 연산자를 입력하세요 : ");
+	scanf_s(" %c", &b);
+	printf("두번쨰 수를 입력하세요 : ");
+	scanf_s("%d", &c);
+
+	if (b == '+')
+	{
+		printf("%d + %d = %d 입니다.\n", a, c, a + c);
+	}
+	if (b == '-')
+	{
+		printf("%d - %d = %d 입니다.\n", a, c, a - c);
+	}
+	if (b == '*')
+	{
+		printf("%d * %d = %d 입니다.\n", a, c, a * c);
+	}
+	if (b == '/')
+	{
+		printf("%d / %d = %d 입니다.\n", a, c, a / c);
+	}
+	if (b == '%')
+	{
+		printf("%d % %d = %d 입니다.\n", a, c, a % c);
+	}
+
+	return 0;
+}
+*/
 //	예제 모음05
+/*
+#include	<stdio.h>
+
+int main(void)
+{
+	int a, c;
+	char b;
+
+	printf("첫번쨰 수를 입력하세요 : ");
+	scanf_s("%d", &a);
+	printf("계산할 연사자를 입력하세요 : ");
+	scanf_s(" %d", &b);
+	printf("두번쨰 수를 입력하세요 : ");
+	scanf_s("%d", &c);
+	
+	if ( b == '+' )
+	{
+		printf("%d + %d = %d", a, c, a + c);
+	}
+	else {
+		if (b == '-')
+		{
+			printf("%d + %d = %d", a, c, a - c);
+		}
+		else {
+			if (b == '*')
+			{
+				printf("%d + %d = %d", a, c, a * c);
+			}
+			else {
+				if (b == '/')
+				{
+					printf("%d + %d = %d", a, c, a / c);
+				}
+				else {
+					if (b == '%')
+					{
+						printf("%d + %d = %d", a, c, a % c);
+					}
+					else {
+						printf("연산자를 잘못 입력 했습니다.");
+					}
+				}
+			}
+		}
+	}
+
+
+}
+*/
+
 //	예제 모음06
+#include	<stdio.h>
+
+int main(void)
+{
+	int a, c;
+	char b;
+
+	printf("수식을 한줄로 띄어쓰기로 입력하세요 : ");
+	scanf_s("%d %c %d", &a, &b, &c);
+
+	switch (b)
+	{
+		case '+':
+			printf("%d + %d = %d 입니다.\n", a, c, a + c);
+			break;
+		case '-':
+			printf("%d - %d = %d 입니다.\n", a, c, a - c);
+			break;
+		case '*':
+			printf("%d * %d = %d 입니다.\n", a, c, a * c);
+			break;
+		case '/':
+			printf("%d / %d = %d 입니다.\n", a, c, a / c);
+			break;
+		case '%':
+			printf("%d % %d = %d 입니다.\n", a, c, a % c);
+			break;
+
+	default:
+		printf("연산자를 잘못 입력했습니다.");
+	}
+}
+
 //	예제 모음07
 //	예제 모음08
 //	예제 모음09
